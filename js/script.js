fdbf49502a28201e6559867e775110b39830dc39
alexrazo13 <alexrazo321@gmail.com>
2022-04-06 22:58:20 -0700

    Commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
-// C.R.U.D.
-// Create
-h2 = document.createElement('h2') // Create a new element
-h2.innerHTML = 'Create'; // write text in element
-document.body.apprendChild(h2); // place element on the body
+// This is a single line comment
 
-// Read
-h1 = document.querySelector('h1'); // select element
-h1.innerHTML = 'Now it is at the bottom'; // write in text element
-document.body.apprendChild(h1); // place element on the body
+/*
+This
+is a
+multi line
+comment
+*/
 
-// Update
-h2.style.color = 'purple'; // change style
-h2.innerHTML = 'Create Elements'
-
-// Delete
-h2.remove(); // remove elements
-document.body.apprend(h2);
